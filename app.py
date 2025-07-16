ab0fbf7f331f31df4312a4fa9ee3330d3eb486e7
Yogan-jp <jp.yoganand@gmail.com>
2025-07-16 05:53:14 +0000

    update stop telegram

--- app.py
+++ app.py
@@ -79,6 +79,23 @@ def telegram():
     status = "The telegram bot is running. Please check @gemini_tt_bot" if webhook_response.status_code == 200 else "Failed to start the telegram bot."
     return render_template("telegram.html", status=status)
 
+@app.route("/stop_telegram",methods=["GET","POST"])
+def stop_telegram():
+
+    domain_url = 'https://dsat-ft1-0e01.onrender.com'
+
+    # The following line is used to delete the existing webhook URL for the Telegram bot
+    delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
+    webhook_response = requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    if webhook_response.status_code == 200:
+        # set status message
+        status = "The telegram bot is stopped. "
+    else:
+        status = "Failed to stop the telegram bot. Please check the logs."
+    
+    return(render_template("telegram.html", status=status))
+
 @app.route("/webhook", methods=["GET", "POST"])
 def webhook():
     update = request.get_json()
